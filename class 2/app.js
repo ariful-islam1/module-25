2b132f67a0c9d0740347fa0a0497a2280935a62f
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-30 12:07:54 +0600

    first commit

---
+++ class 2/app.js
@@ -0,0 +1,27 @@
+const greenBtn = document.getElementById('green');
+greenBtn.style.backgroundColor = 'green';
+greenBtn.style.padding = '20px';
+greenBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'green';
+})
+
+const cyanBtn = document.getElementById('cyan');
+cyanBtn.style.backgroundColor = 'cyan';
+cyanBtn.style.padding = '20px';
+cyanBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'cyan';
+})
+
+const purpleBtn = document.getElementById('purple');
+purpleBtn.style.backgroundColor = 'purple';
+purpleBtn.style.padding = '20px';
+purpleBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'purple';
+})
+
+const orangeBtn = document.getElementById('orange');
+orangeBtn.style.backgroundColor = 'orange';
+orangeBtn.style.padding = '20px';
+orangeBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'orange';
+})
